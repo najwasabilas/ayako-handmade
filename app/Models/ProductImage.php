8960aa0c35bd2d158cb81fa513b43676201e7326
najwasabilas <najwa.sabila2546@student.unri.ac.id>
2025-09-09 15:38:11 +0700

    creating migration and model

---
+++ app/Models/ProductImage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductImage extends Model {
+    protected $fillable = ['product_id','gambar'];
+
+    public function product() {
+        return $this->belongsTo(Product::class);
+    }
+}
