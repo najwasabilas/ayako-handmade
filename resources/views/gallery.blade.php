22772464d09e1dd1623139c43a59f273b6ef86d2
rahmahmuthmainah <rahmah.muthmainah3281@student.unri.ac.id>
2025-11-28 12:14:13 +0700

    Update Gallery

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -19,42 +19,42 @@
         <img src="{{ asset('assets/gallery1.png') }}" alt="Persiapan Bahan">
         <div class="gallery-info">
           <h3>Persiapan Bahan</h3>
-          <p>Lorem ipsum dolor sit amet consectetur. In sagittis lacus gravida scelerisque elementum posuere est sit.</p>
-        </div>
+          <p>seluruh bahan dasar seperti kain dan aksesoris pendukung diseleksi untuk memastikan kualitas terbaik sebelum proses produksi dimulai.
+          </div>
       </div>
       <div class="gallery-card">
         <img src="{{ asset('assets/gallery2.png') }}" alt="Pemotongan Kain">
         <div class="gallery-info">
           <h3>Pemotongan Kain</h3>
-          <p>Lorem ipsum dolor sit amet consectetur. In sagittis lacus gravida scelerisque elementum posuere est sit.</p>
+          <p>Kain dan material lainnya dipotong sesuai pola agar setiap bagian tas memiliki ukuran yang presisi dan siap dirangkai ke tahap berikutnya.</p>
         </div>
       </div>
       <div class="gallery-card">
         <img src="{{ asset('assets/gallery3.png') }}" alt="Penjahitan">
         <div class="gallery-info">
           <h3>Penjahitan</h3>
-          <p>Lorem ipsum dolor sit amet consectetur. In sagittis lacus gravida scelerisque elementum posuere est sit.</p>
+          <p>Setiap potongan bahan dijahit dengan teliti untuk menghasilkan tas yang kuat, rapi, dan memiliki nilai estetika tinggi.</p>
         </div>
       </div>
       <div class="gallery-card">
         <img src="{{ asset('assets/gallery4.png') }}" alt="Pemasangan Fitur Tambahan">
         <div class="gallery-info">
           <h3>Pemasangan Fitur Tambahan</h3>
-          <p>Lorem ipsum dolor sit amet consectetur. In sagittis lacus gravida scelerisque elementum posuere est sit.</p>
+          <p>Fitur tambahan seperti resleting, tali, dan ornamen dekoratif dipasang untuk memberikan fungsi ekstra sekaligus mempercantik tampilan tas.</p>
         </div>
       </div>
       <div class="gallery-card">
         <img src="{{ asset('assets/gallery5.png') }}" alt="Penempelan Logo">
         <div class="gallery-info">
           <h3>Penempelan Logo</h3>
-          <p>Lorem ipsum dolor sit amet consectetur. In sagittis lacus gravida scelerisque elementum posuere est sit.</p>
+          <p>Logo merek ditempel secara hati-hati agar melekat sempurna dan menjadi identitas resmi pada setiap produk yang dihasilkan.</p>
         </div>
       </div>
       <div class="gallery-card">
         <img src="{{ asset('assets/gallery6.png') }}" alt="Pengemasan">
         <div class="gallery-info">
           <h3>Pengemasan</h3>
-          <p>Lorem ipsum dolor sit amet consectetur. In sagittis lacus gravida scelerisque elementum posuere est sit.</p>
+          <p>Produk yang telah selesai melalui proses pengecekan kualitas kemudian dikemas rapi untuk memastikan aman sampai ke tangan pelanggan.</p>
         </div>
       </div>
 
@@ -71,7 +71,7 @@
     <div class="testimonial-text">
       <p class="quote-icon">“</p>
       <p class="quote-text">
-        Lorem ipsum dolor sit amet consectetur. In sagittis lacus gravida scelerisque elementum posuere est sit. Porta cras est vulputate sit at. Facilisis scelerisque a quam sed. Euismod nec turpis justo aliquam bibendum.
+       Setiap tas yang kami hasilkan adalah cerminan budaya Riau dan kerja tangan penuh ketulusan dari ibu-ibu rumah tangga yang kami berdayakan. Kami percaya, produk terbaik lahir dari hati yang bekerja bersama.
       </p>
       <p class="quote-icon right">”</p>
     </div>
